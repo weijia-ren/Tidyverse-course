735b60c5a7bb93a1743afeed99ca488697d42090
Weijia <WeijiaRen@westat.com>
2019-09-25 17:15:31 -0400

    initial update

---
+++ install-instructions.R
@@ -0,0 +1,7 @@
+# Run the code below on your own computer to 
+# install the packages that we used in class today.
+
+install.packages(c("babynames", "formatR", "gapminder", 
+                   "hexbin", "mgcv", "maps", "mapproj", 
+                   "nycflights13", "rmarkdown", "tidyverse", 
+                   "viridis"))
